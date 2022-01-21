578ccbbdb0d829d5dcb63c7b3bbf890968aabe05
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-21 15:02:41 -0500

    Async and sync updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sync(file) {
   const fs = require("fs");
   const data = fs.readFileSync("data.json", "utf-8");
   console.log(data);
-  return file;
+  //   return file;
 }
 sync();
 
@@ -13,11 +13,14 @@ sync();
 function async() {
   const fs = require("fs");
   const data1 = fs.readFile("data.json", (err, data1) => {
-    //for async. call backs you wont have access to it except through in there
+    //for async. call backs..you wont have access to something you need (ie. data1) except through in the call back.
     if (err) throw err;
-    console.log(data1.toString("utf-8"));
+    // console.log(data1.toString("utf-8"));
   });
   return "done";
   //return file;
 }
 async();
+
+//Promises
+//
